7081fba7c5da01f3ded990ccf89aa528b102c000
Julian Barbosa <bjvlixn@gmail.com>
2022-04-01 01:29:47 -0300

    add Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import style from "./Footer.module.scss";
+console.log('style', style)
+
+export const Footer = () => {
+  return (
+    <p className={style.paragraph}>
+      made with ❤️ by{" "}
+      <a
+        href="https://github.com/julianFBarbosa"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Julian Barbosa
+      </a>
+    </p>
+  );
+};
