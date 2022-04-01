ebe67e226af7890a2e848be92118b6d2416f1c3b
Julian Barbosa <bjvlixn@gmail.com>
2022-04-01 01:28:57 -0300

    add Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react";
-// import styles from "./Header.module.scss";
-import Helmet from "react-helmet";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const PostList = () => {
+import { SearchBar } from "../SearchBar";
+
+export const Header = () => {
   return (
-    <div>
-      <Helmet>
-        <title>Lojinha | Busque um produto</title>
-      </Helmet>
-      <div>
-        <label htmlFor="">
-          <input type="text" />
-        </label>
-        <p>Não há nenhuma notícia cadastrada :(</p>
-      </div>
-    </div>
+    <Card className="my-3">
+      <Card.Body className="d-flex justify-content-start justify-content-md-between align-items-center flex-column flex-md-row">
+        <Card.Title>
+          <Link to="/" className="text-dark text-decoration-none">Lojinha</Link>
+        </Card.Title>
+        <SearchBar />
+      </Card.Body>
+    </Card>
   );
 };
