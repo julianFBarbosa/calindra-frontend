2193c9147ae4243cd2afd77f784d9a1088697701
Julian Barbosa <bjvlixn@gmail.com>
2022-04-01 01:29:07 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Route, Routes as RouteList } from "react-router-dom";
-import { PostList } from "./components/PostList";
+import { Home } from "./components/Home";
+import { ProductList } from "./components/ProductList";
 
 export const Routes = () => (
   <RouteList>
-    <Route path="/" element={<PostList />} />
+    <Route path="/" element={<Home />} />
+    <Route path="/produtos/:name" element={<ProductList />} />
   </RouteList>
 );
